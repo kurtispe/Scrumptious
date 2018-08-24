c5ffa394103d7bda26871ab64a1bf41a97c467be
ccarrjoh <courtneyjohnson928@gmail.com>
2018-08-24 16:08:18 -0400

    controllers

--- src/Scrumptious.MvcClient/Controllers/TaskController.cs
+++ src/Scrumptious.MvcClient/Controllers/TaskController.cs
@@ -5,22 +5,34 @@ using Newtonsoft.Json;
 using Scrumptious.MvcClient.Models;
 using System.Threading;
 using System.Text;
-namespace Scrumptious.Mvclient.Controllers
+
+namespace Scrumptious.MvcClient.Controllers
 {
     [Route("[controller]")]
     [Produces("application/json")]
     public class TaskController : Controller
     {
         private readonly HttpClient http = new HttpClient();
-
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             var x = await http.GetAsync("http://localhost:62021/api/task/1");
-            var content = JsonConvert.DeserializeObject<TaskViewModel>(await x.Content.ReadAsStringAsync());
-            ViewData["pagetitle"] = "List of Tasks";
+            var content = JsonConvert.DeserializeObject<SprintViewModel>(await x.Content.ReadAsStringAsync());
+            ViewData["pagetitle"] = "Scrumptious";
+            ViewBag.Title = "Scrumptious, the Scrum Master Program!";
             ViewBag.content = content;
+            ViewBag.userQuery = false;
             return View();
+        }
+
+        [HttpGet("{sort}")]
+        public IActionResult Get(string id)
+        {
+            ViewData["pagetitle"] = "Scrumptious";
+            ViewBag.Title = "Scrumptious, the Scrum Master Program!";
+            string s = Request.Query["ID"];
+            ViewBag.userQuery = true;
+            return Redirect("/task/" + s);
         }
 
         [HttpGet("{id:int}")]
@@ -28,23 +40,19 @@ namespace Scrumptious.Mvclient.Controllers
         {
             var x = await http.GetAsync("http://localhost:62021/api/task/" + id);
             var content = JsonConvert.DeserializeObject<TaskViewModel>(await x.Content.ReadAsStringAsync());
-            ViewData["pagetitle"] = "List of Tasks";
+            ViewData["pagetitle"] = "Scrumptious";
+            ViewBag.Title = "Scrumptious, the Scrum Master Program!";
             ViewBag.content = content;
+            ViewBag.userQuery = true;
             return View();
         }
 
         [HttpPost]
-        public void Post()
+        public IActionResult Post(TaskViewModel data)
         {
-            var pvm = new TaskViewModel()
-            {
-                Name = "billy bob",
-                Completed = false,
-                Requirements = "some desc",
-                TaskDescription = "something works"
-            };
-            var content = JsonConvert.SerializeObject(pvm);
+            var content = JsonConvert.SerializeObject(data);
             http.PostAsync("http://localhost:62021/api/task", new StringContent(content, Encoding.UTF8, "application/json"));
+            return Redirect("/task");
         }
 
     }
